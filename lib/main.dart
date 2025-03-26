4e3b7b32f4f9523c2c186bc2c117a83ed3e88689
jeonlego012 <jyo9873@gmail.com>
2025-03-26 23:38:01 +0900

    brick breaker game test using flame

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:pedometer/game/brick_breaker.dart';
 import 'package:pedometer/step/application/step_bloc.dart' as bloc;
 
 void main() {
-  runApp(const MyApp());
+  final game = BrickBreaker();
+  runApp(GameWidget(game: game));
+  //runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
