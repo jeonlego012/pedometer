95184c61dc0f1255701ada025c2fcf42d423a459
jeonlego012 <jyo9873@gmail.com>
2025-03-27 23:10:12 +0900

    modify game difficulty

--- lib/game/brick_breaker.dart
+++ lib/game/brick_breaker.dart
@@ -80,7 +80,7 @@ class BrickBreaker extends FlameGame
     );
     world.addAll([
       for (var i = 0; i < brickColors.length; i++)
-        for (var j = 1; j <= 2; j++)
+        for (var j = 1; j <= 5; j++)
           Brick(
             position: Vector2(
               (i + 0.5) * brickWidth + (i + 1) * brickGutter,
